9428926a46e78bab18c2bafad17f8d3d28056179
Dustin Horvath <dustinmhorvath@gmail.com>
2017-04-12 12:56:53 -0500

    First 2 exercises done (I think), but no real testing of scoping has been done.

---
+++ proj3/proj3Utils.hs
@@ -0,0 +1,256 @@
+{-# LANGUAGE GADTs #-}
+
+module Proj3Utils where
+
+-- Imports for QuickCheck
+import System.Random
+import Test.QuickCheck
+import Test.QuickCheck.Gen
+import Test.QuickCheck.Function
+import Test.QuickCheck.Monadic
+
+-- Imports for Parsec
+import Control.Monad
+import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec.Language
+import Text.ParserCombinators.Parsec.Expr
+import Text.ParserCombinators.Parsec.Token
+
+-- Imports for PLIH
+import ParserUtils
+
+--
+-- Project utilities for developing CFAE and CFBAE
+-- interpreters.
+--
+-- Author: Perry Alexander
+-- Date: 6 April 2017
+--
+
+-- CFAE AST Definition
+
+data CFAE where
+  Num :: Int -> CFAE
+  Plus :: CFAE -> CFAE -> CFAE
+  Minus :: CFAE -> CFAE -> CFAE
+  Mult :: CFAE -> CFAE -> CFAE
+  Div :: CFAE -> CFAE -> CFAE
+  Lambda :: String -> CFAE -> CFAE
+  App :: CFAE -> CFAE -> CFAE
+  Id :: String -> CFAE
+  If0 :: CFAE -> CFAE -> CFAE -> CFAE
+  deriving (Show,Eq)
+
+
+
+type Env = [(String,CFAE)]
+--type Cont = [(String,TFBAE)]
+
+-- Parser
+
+expr :: Parser CFAE
+expr = buildExpressionParser opTable term
+
+opTable = [ [ inFix "*" Mult AssocLeft
+            , inFix "/" Div AssocLeft ]
+          , [ inFix "+" Plus AssocLeft
+            , inFix "-" Minus AssocLeft ]
+          ]
+
+numExpr :: Parser CFAE
+numExpr = do i <- integer lexer
+             return (Num (fromInteger i))
+
+identExpr :: Parser CFAE
+identExpr = do i <- identifier lexer
+               return (Id i)
+              
+lambdaExpr :: Parser CFAE
+lambdaExpr = do reserved lexer "lambda"
+                i <- identifier lexer
+                reserved lexer "in"
+                e <- expr
+                return (Lambda i e)
+
+appExpr :: Parser CFAE
+appExpr = do reserved lexer "app"
+             e1 <- expr
+             e2 <- expr
+             return (App e1 e2)
+
+if0Expr :: Parser CFAE
+if0Expr = do reserved lexer "if0"
+             c <- expr
+             reserved lexer "then"
+             t <- expr
+             reserved lexer "else"
+             e <- expr
+             return (If0 c t e)
+            
+             
+term = parens lexer expr
+       <|> numExpr
+       <|> identExpr
+       <|> if0Expr
+       <|> lambdaExpr
+       <|> appExpr
+             
+-- Parser invocation
+
+parseCFAE = parseString expr
+
+parseCFAEFile = parseFile expr
+
+
+-- EXERCISE 1
+
+evalDynCFAE :: Env -> CFAE -> CFAE
+evalDynCFAE env (Num x) = (Num x)
+
+evalDynCFAE env (Plus t1 t2) = let (Num v1) = (evalDynCFAE env t1)
+                                   (Num v2) = (evalDynCFAE env t2)
+                              in (Num (v1+v2))
+evalDynCFAE env (Minus t1 t2) = let (Num v1) = (evalDynCFAE env t1)
+                                    (Num v2) = (evalDynCFAE env t2)
+                                in (Num (v1-v2))
+
+
+evalDynCFAE env (Mult t1 t2) = let (Num v1) = (evalDynCFAE env t1)
+                                   (Num v2) = (evalDynCFAE env t2)
+                               in (Num (v1*v2))
+evalDynCFAE env (Div t1 t2) = let (Num v1) = (evalDynCFAE env t1)
+                                  (Num v2) = (evalDynCFAE env t2)
+                              in (Num (div v1 v2))
+evalDynCFAE env (If0 t1 t2 t3) = let (Num v1) = (evalDynCFAE env t1)
+                                     (Num v2) = (evalDynCFAE env t2)
+                                     (Num v3) = (evalDynCFAE env t3)
+                                 in case v1 of
+                                   0 -> (Num v2)
+                                   _ -> (Num v3)
+evalDynCFAE env (Lambda i b) = (Lambda i b)
+evalDynCFAE env (App f a) =  let (Lambda i b) = (evalDynCFAE env f)
+                                 a' = (evalDynCFAE env a) 
+                             in evalDynCFAE ((i,a'):env) b
+evalDynCFAE env (Id id) = case (lookup id env) of
+                            Just x -> x
+                            Nothing -> error "Varible not found"
+
+interpDynCFAE :: String -> CFAE
+interpDynCFAE = (evalDynCFAE []) . parseCFAE
+
+-- EXERCISE 2
+
+-- CCFAE Parser
+-- (with (C)losures)CFAE
+
+-- Parser
+
+exprX :: Parser CCFAE
+exprX = buildExpressionParser opTableX termX
+
+opTableX = [ [ inFix "*" MultX AssocLeft
+            , inFix "/" DivX AssocLeft ]
+          , [ inFix "+" PlusX AssocLeft
+            , inFix "-" MinusX AssocLeft ]
+          ]
+
+numExprX :: Parser CCFAE
+numExprX = do i <- integer lexer
+              return (NumX (fromInteger i))
+
+identExprX :: Parser CCFAE
+identExprX = do i <- identifier lexer
+                return (IdX i)
+
+bindExprX :: Parser CCFAE
+bindExprX = do reserved lexer "bind"
+               i <- identifier lexer
+               reservedOp lexer "="
+               v <- exprX
+               reserved lexer "in"
+               e <- exprX
+               return (BindX i v e)
+              
+lambdaExprX :: Parser CCFAE
+lambdaExprX = do reserved lexer "lambda"
+                 i <- identifier lexer
+                 reserved lexer "in"
+                 e <- exprX
+                 return (LambdaX i e)
+
+appExprX :: Parser CCFAE
+appExprX = do reserved lexer "app"
+              e1 <- exprX
+              e2 <- exprX
+              return (AppX e1 e2)
+
+ifExprX :: Parser CCFAE
+ifExprX = do reserved lexer "if"
+             c <- exprX
+             reserved lexer "then"
+             t <- exprX
+             reserved lexer "else"
+             e <- exprX
+             return (IfX c t e)
+            
+             
+termX = parens lexer exprX
+       <|> numExprX
+       <|> identExprX
+       <|> bindExprX
+       <|> ifExprX
+       <|> lambdaExprX
+       <|> appExprX
+             
+-- Parser invocation
+
+parseCCFAE = parseString exprX
+
+parseCCFAEFile = parseFile exprX
+
+-- CCFAE AST Definition
+
+data CCFAE where
+  NumX :: Int -> CCFAE
+  PlusX :: CCFAE -> CCFAE -> CCFAE
+  MinusX :: CCFAE -> CCFAE -> CCFAE
+  MultX :: CCFAE -> CCFAE -> CCFAE
+  DivX :: CCFAE -> CCFAE -> CCFAE
+  BindX :: String -> CCFAE -> CCFAE -> CCFAE
+  LambdaX :: String -> CCFAE -> CCFAE
+  AppX :: CCFAE -> CCFAE -> CCFAE
+  IdX :: String -> CCFAE
+  IfX :: CCFAE -> CCFAE -> CCFAE -> CCFAE
+  Closure :: String -> CCFAE -> CEnv -> CCFAE
+  deriving (Show,Eq)
+
+type CEnv = [(String,CCFAEVal)]
+
+data CCFAEVal where
+  NumV :: Int -> CCFAEVal
+  ClosureV :: String -> CCFAE -> CEnv -> CCFAEVal
+  deriving (Show,Eq)
+
+
+evalStatCFBE :: CEnv -> CCFAE -> CCFAEVal
+evalStatCFBE cenv (NumX x) = (NumV x)
+evalStatCFBE cenv (PlusX l r) = let (NumV l') = (evalStatCFBE cenv l)
+                                    (NumV r') = (evalStatCFBE cenv r)
+                                in (NumV (l'+r'))
+evalStatCFBE cenv (MinusX l r) = let (NumV l') = (evalStatCFBE cenv l)
+                                     (NumV r') = (evalStatCFBE cenv r)
+                                 in (NumV (l'-r'))
+evalStatCFBE cenv (LambdaX i b) = (ClosureV i b cenv)
+evalStatCFBE cenv (AppX f a) = let (ClosureV i b e) = (evalStatCFBE cenv f)
+                                   a' = (evalStatCFBE cenv a)
+                               in evalStatCFBE ((i,a'):e) b
+evalStatCFBE cenv (IdX id) = case (lookup id cenv) of
+                               Just x -> x
+                               Nothing -> error "Varible not found"
+evalStatCFBE cenv (IfX c t e) = let (NumV c') = (evalStatCFBE cenv c) in
+                                  if c'==0 then (evalStatCFBE cenv t) else (evalStatCFBE cenv e)
+
+-- We're getting values out at the end now
+interpStatCCFAE :: String -> CCFAEVal
+interpStatCCFAE = (evalStatCFBE []) . parseCCFAE
+
