7713d0d11015866e7b760822ffcc39697fd7d313
Dustin Horvath <dustinmhorvath@gmail.com>
2017-04-12 13:33:57 -0500

    Made a booboo with starting variables with uppercase, fixed.

--- proj3/proj3Utils.hs
+++ proj3/proj3Utils.hs
@@ -42,7 +42,6 @@ data CFAE where
   deriving (Show,Eq)
 
 
-
 type Env = [(String,CFAE)]
 --type Cont = [(String,TFBAE)]
 
@@ -140,117 +139,183 @@ interpDynCFAE = (evalDynCFAE []) . parseCFAE
 
 -- EXERCISE 2
 
--- CCFAE Parser
--- (with (C)losures)CFAE
 
 -- Parser
 
-exprX :: Parser CCFAE
-exprX = buildExpressionParser opTableX termX
+-- CFAEb AST Definition
 
-opTableX = [ [ inFix "*" MultX AssocLeft
-            , inFix "/" DivX AssocLeft ]
-          , [ inFix "+" PlusX AssocLeft
-            , inFix "-" MinusX AssocLeft ]
-          ]
+data CFAEb where
+  CNum :: Int -> CFAEb
+  CPlus :: CFAEb -> CFAEb -> CFAEb
+  CMinus :: CFAEb -> CFAEb -> CFAEb
+  CMult :: CFAEb -> CFAEb -> CFAEb
+  CDiv :: CFAEb -> CFAEb -> CFAEb
+  CBind :: String -> CFAEb -> CFAEb -> CFAEb
+  CLambda :: String -> CFAEb -> CFAEb
+  CApp :: CFAEb -> CFAEb -> CFAEb
+  CId :: String -> CFAEb
+  CIf :: CFAEb -> CFAEb -> CFAEb -> CFAEb
+  Closure :: String -> CFAEb -> CEnv -> CFAEb
+  deriving (Show,Eq)
 
-numExprX :: Parser CCFAE
-numExprX = do i <- integer lexer
-              return (NumX (fromInteger i))
+exprC :: Parser CFAEb
+exprC = buildExpressionParser opTableC termC
 
-identExprX :: Parser CCFAE
-identExprX = do i <- identifier lexer
-                return (IdX i)
+opTableC = [ [ inFix "*" CMult AssocLeft
+             , inFix "/" CDiv AssocLeft ]
+           , [ inFix "+" CPlus AssocLeft
+             , inFix "-" CMinus AssocLeft ]
+           ]
 
-bindExprX :: Parser CCFAE
-bindExprX = do reserved lexer "bind"
+numExprC :: Parser CFAEb
+numExprC = do i <- integer lexer
+              return (CNum (fromInteger i))
+
+identExprC :: Parser CFAEb
+identExprC = do i <- identifier lexer
+                return (CId i)
+
+bindExprC :: Parser CFAEb
+bindExprC = do reserved lexer "bind"
                i <- identifier lexer
                reservedOp lexer "="
-               v <- exprX
+               v <- exprC
                reserved lexer "in"
-               e <- exprX
-               return (BindX i v e)
+               e <- exprC
+               return (CBind i v e)
               
-lambdaExprX :: Parser CCFAE
-lambdaExprX = do reserved lexer "lambda"
+lambdaExprC :: Parser CFAEb
+lambdaExprC = do reserved lexer "lambda"
                  i <- identifier lexer
                  reserved lexer "in"
-                 e <- exprX
-                 return (LambdaX i e)
+                 e <- exprC
+                 return (CLambda i e)
 
-appExprX :: Parser CCFAE
-appExprX = do reserved lexer "app"
-              e1 <- exprX
-              e2 <- exprX
-              return (AppX e1 e2)
+appExprC :: Parser CFAEb
+appExprC = do reserved lexer "app"
+              e1 <- exprC
+              e2 <- exprC
+              return (CApp e1 e2)
 
-ifExprX :: Parser CCFAE
-ifExprX = do reserved lexer "if"
-             c <- exprX
+ifExprC :: Parser CFAEb
+ifExprC = do reserved lexer "if"
+             c <- exprC
              reserved lexer "then"
-             t <- exprX
+             t <- exprC
              reserved lexer "else"
-             e <- exprX
-             return (IfX c t e)
+             e <- exprC
+             return (CIf c t e)
             
              
-termX = parens lexer exprX
-       <|> numExprX
-       <|> identExprX
-       <|> bindExprX
-       <|> ifExprX
-       <|> lambdaExprX
-       <|> appExprX
+termC = parens lexer exprC
+        <|> numExprC
+        <|> identExprC
+        <|> bindExprC
+        <|> ifExprC
+        <|> lambdaExprC
+        <|> appExprC
              
 -- Parser invocation
 
-parseCCFAE = parseString exprX
+parseCFAEb = parseString exprC
 
-parseCCFAEFile = parseFile exprX
+parseCFAEbFile = parseFile exprC
 
--- CCFAE AST Definition
 
-data CCFAE where
-  NumX :: Int -> CCFAE
-  PlusX :: CCFAE -> CCFAE -> CCFAE
-  MinusX :: CCFAE -> CCFAE -> CCFAE
-  MultX :: CCFAE -> CCFAE -> CCFAE
-  DivX :: CCFAE -> CCFAE -> CCFAE
-  BindX :: String -> CCFAE -> CCFAE -> CCFAE
-  LambdaX :: String -> CCFAE -> CCFAE
-  AppX :: CCFAE -> CCFAE -> CCFAE
-  IdX :: String -> CCFAE
-  IfX :: CCFAE -> CCFAE -> CCFAE -> CCFAE
-  Closure :: String -> CCFAE -> CEnv -> CCFAE
-  deriving (Show,Eq)
+type CEnv = [(String,CFAEbVal)]
 
-type CEnv = [(String,CCFAEVal)]
-
-data CCFAEVal where
-  NumV :: Int -> CCFAEVal
-  ClosureV :: String -> CCFAE -> CEnv -> CCFAEVal
+data CFAEbVal where
+  NumV :: Int -> CFAEbVal
+  ClosureV :: String -> CFAEb -> CEnv -> CFAEbVal
   deriving (Show,Eq)
 
 
-evalStatCFBE :: CEnv -> CCFAE -> CCFAEVal
-evalStatCFBE cenv (NumX x) = (NumV x)
-evalStatCFBE cenv (PlusX l r) = let (NumV l') = (evalStatCFBE cenv l)
+evalStatCFBE :: CEnv -> CFAEb -> CFAEbVal
+evalStatCFBE cenv (CNum x) = (NumV x)
+evalStatCFBE cenv (CPlus l r) = let (NumV l') = (evalStatCFBE cenv l)
                                     (NumV r') = (evalStatCFBE cenv r)
                                 in (NumV (l'+r'))
-evalStatCFBE cenv (MinusX l r) = let (NumV l') = (evalStatCFBE cenv l)
+evalStatCFBE cenv (CMinus l r) = let (NumV l') = (evalStatCFBE cenv l)
                                      (NumV r') = (evalStatCFBE cenv r)
                                  in (NumV (l'-r'))
-evalStatCFBE cenv (LambdaX i b) = (ClosureV i b cenv)
-evalStatCFBE cenv (AppX f a) = let (ClosureV i b e) = (evalStatCFBE cenv f)
+evalStatCFBE cenv (CLambda i b) = (ClosureV i b cenv)
+evalStatCFBE cenv (CApp f a) = let (ClosureV i b e) = (evalStatCFBE cenv f)
                                    a' = (evalStatCFBE cenv a)
                                in evalStatCFBE ((i,a'):e) b
-evalStatCFBE cenv (IdX id) = case (lookup id cenv) of
+evalStatCFBE cenv (CId id) = case (lookup id cenv) of
                                Just x -> x
                                Nothing -> error "Varible not found"
-evalStatCFBE cenv (IfX c t e) = let (NumV c') = (evalStatCFBE cenv c) in
+evalStatCFBE cenv (CIf c t e) = let (NumV c') = (evalStatCFBE cenv c) in
                                   if c'==0 then (evalStatCFBE cenv t) else (evalStatCFBE cenv e)
 
 -- We're getting values out at the end now
-interpStatCCFAE :: String -> CCFAEVal
-interpStatCCFAE = (evalStatCFBE []) . parseCCFAE
+interpStatCFAEb :: String -> CFAEbVal
+interpStatCFAEb = (evalStatCFBE []) . parseCFAEb
 
+
+
+-- EXERCISE 3
+
+--exprX :: Parser CCFAE
+--exprX = buildExpressionParser opTableX termX
+--
+--opTableX = [ [ inFix "*" MultX AssocLeft
+--            , inFix "/" DivX AssocLeft ]
+--          , [ inFix "+" PlusX AssocLeft
+--            , inFix "-" MinusX AssocLeft ]
+--          ]
+--
+--numExprX :: Parser CCFAE
+--numExprX = do i <- integer lexer
+--              return (NumX (fromInteger i))
+--
+--identExprX :: Parser CCFAE
+--identExprX = do i <- identifier lexer
+--                return (IdX i)
+--
+--bindExprX :: Parser CCFAE
+--bindExprX = do reserved lexer "bind"
+--               i <- identifier lexer
+--               reservedOp lexer "="
+--               v <- exprX
+--               reserved lexer "in"
+--               e <- exprX
+--               return (BindX i v e)
+--              
+--lambdaExprX :: Parser CCFAE
+--lambdaExprX = do reserved lexer "lambda"
+--                 i <- identifier lexer
+--                 reserved lexer "in"
+--                 e <- exprX
+--                 return (LambdaX i e)
+--
+--appExprX :: Parser CCFAE
+--appExprX = do reserved lexer "app"
+--              e1 <- exprX
+--              e2 <- exprX
+--              return (AppX e1 e2)
+--
+--ifExprX :: Parser CCFAE
+--ifExprX = do reserved lexer "if"
+--             c <- exprX
+--             reserved lexer "then"
+--             t <- exprX
+--             reserved lexer "else"
+--             e <- exprX
+--             return (IfX c t e)
+--            
+--             
+--termX = parens lexer exprX
+--       <|> numExprX
+--       <|> identExprX
+--       <|> bindExprX
+--       <|> ifExprX
+--       <|> lambdaExprX
+--       <|> appExprX
+--             
+---- Parser invocation
+--
+----parseCCFAE = parseString exprX
+--
+----parseCCFAEFile = parseFile exprX
+--
